42f752949244681762536dd9695fee8bb7a4df26
Ayobami Agunroye <ayobamiagunroye@gmail.com>
2022-09-21 01:34:07 +0100

    feat: add ability to upload and delete review image, add delete review functionality

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const reviewController = require("../controllers/reviewsController");
+const { multerUploadsMultiple } = require("../utils/multer");
 
 const router = express.Router();
 
@@ -10,5 +11,12 @@ router.get("/:id", reviewController.getOneReview);
 router.get("/places-review/:id", reviewController.getReviewsForAllPlace);
 router.patch("/edit-likes/:id", reviewController.increaseOrReduceReviewsLikes);
 router.patch("/:id", reviewController.editReviewDetails);
+router.patch(
+  "/:id/add-images",
+  multerUploadsMultiple,
+  reviewController.addReviewImages
+);
+router.patch("/:id/remove-images", reviewController.removeReviewImages);
+router.delete("/:id", reviewController.deleteReview);
 
 module.exports = router;
